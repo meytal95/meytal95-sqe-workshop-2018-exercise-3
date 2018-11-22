aeb45c25a35d93fbac356e9a587a1be8d1605461
meytal95 <meytalbu@post.bgu.ac.il>
2018-11-22 13:43:07 +0200

    s

--- src/js/code-analyzer.js
+++ src/js/code-analyzer.js
@@ -1,8 +1,8 @@
 import * as esprima from 'esprima';
-import * as barakgay from 'escodegen';
+import * as escodegen from 'escodegen';
 
-let thing=[];
-let amElse=false;
+let thing = [];
+let amElse = false;
 
 const parseTypes = {
     'VariableDeclaration': parseVarDecl,
@@ -10,45 +10,43 @@ const parseTypes = {
     'FunctionDeclaration': parseFunc,
     'WhileStatement': parseWhileStmt,
     'IfStatement': parseIf,
-    'BlockStatement' : parseBlock,
+    'BlockStatement': parseBlock,
     'ForStatement': parseFor,
-    'ExpressionStatement' : parseExpr,
-    //'UpdateExpression': parseUpdateExpr,
+    'ExpressionStatement': parseExpr,
     'AssignmentExpression': parseAssExpr
 };
 
 const parseCode = (codeToParse) => {
-    thing=[];
-    let data=(esprima.parseScript(codeToParse, {loc: true}).body);
-    //return data;
-    if (data.length===0)
+    thing = [];
+    let data = (esprima.parseScript(codeToParse, {loc: true}).body);
+    if (data.length === 0)
         return thing;
-    barakhomo(data[0]);
+    makeArr(data[0]);
     return (thing);
 };
 
-function barakhomo(data) {
+function makeArr(data) {
     let x = parseTypes[data.type];
     return x(data);
 }
 
 function parseBlock(data) {
-    data.body.map(barakhomo);
+    data.body.map(makeArr);
 }
 
 function parseExpr(data) {
-    barakhomo(data.expression);
+    makeArr(data.expression);
 }
 
 function parseVarDecl(data) {
-    let decs=data.declarations;
-    for(var x of decs) {
+    let decs = data.declarations;
+    for (var x of decs) {
         let vdecl = [5];
         vdecl[0] = x.id.loc.start.line;
-        vdecl[1] = x.type;
+        vdecl[1] = 'variable declaration';
         vdecl[2] = x.id.name;
         vdecl[3] = '';
-        vdecl[4] =(x.init===null) ? '' : barakgay.generate(x.init);
+        vdecl[4] = (x.init === null) ? '' : escodegen.generate(x.init);
         thing.push(vdecl);
     }
 }
@@ -56,10 +54,10 @@ function parseVarDecl(data) {
 function parseRetStmt(data) {
     let vdecl = [5];
     vdecl[0] = data.loc.start.line;
-    vdecl[1] = data.type;
+    vdecl[1] = 'return statement';
     vdecl[2] = '';
     vdecl[3] = '';
-    vdecl[4] =barakgay.generate(data.argument);
+    vdecl[4] = escodegen.generate(data.argument);
     thing.push(vdecl);
 
 }
@@ -67,19 +65,19 @@ function parseRetStmt(data) {
 function parseFunc(data) {
     let vdecl = [5];
     vdecl[0] = data.id.loc.start.line;
-    vdecl[1] = data.type;
+    vdecl[1] = 'function declaration';
     vdecl[2] = data.id.name;
     vdecl[3] = '';
     vdecl[4] = '';
     thing.push(vdecl);
     data.params.map(parseParams);
-    data.body.body.map(barakhomo);
+    data.body.body.map(makeArr);
 }
 
 function parseParams(data) {
     let vdecl = [5];
     vdecl[0] = data.loc.start.line;
-    vdecl[1] = 'VariableDeclaration';
+    vdecl[1] = 'variable declaration';
     vdecl[2] = data.name;
     vdecl[3] = '';
     vdecl[4] = '';
@@ -89,50 +87,49 @@ function parseParams(data) {
 function parseWhileStmt(data) {
     let vdecl = [5];
     vdecl[0] = data.loc.start.line;
-    vdecl[1] = data.type;
+    vdecl[1] = 'while statement';
     vdecl[2] = '';
-    vdecl[3] = barakgay.generate(data.test);
+    vdecl[3] = escodegen.generate(data.test);
     vdecl[4] = '';
     thing.push(vdecl);
-    data.body.body.map(barakhomo);
+    data.body.body.map(makeArr);
 }
 
 function parseIf(data) {
     let vdecl = [5];
     vdecl[0] = data.loc.start.line;
-    vdecl[1] = (amElse)? 'ElseIfStatement' : data.type;
+    vdecl[1] = (amElse) ? 'else if statement' : 'if statement';
     vdecl[2] = '';
-    vdecl[3] = barakgay.generate(data.test);
+    vdecl[3] = escodegen.generate(data.test);
     vdecl[4] = '';
     thing.push(vdecl);
-    amElse=false;
-    barakhomo(data.consequent);
-    if (data.alternate !== null)
-    {
-        amElse=true;
-        barakhomo(data.alternate);
-        amElse=false;
+    amElse = false;
+    makeArr(data.consequent);
+    if (data.alternate !== null) {
+        amElse = true;
+        makeArr(data.alternate);
+        amElse = false;
     }
 }
 
 function parseFor(data) {
     let vdecl = [5];
     vdecl[0] = data.loc.start.line;
-    vdecl[1] = data.type;
+    vdecl[1] = 'for statement';
     vdecl[2] = '';
-    vdecl[3] = barakgay.generate(data.init)+ ';' + barakgay.generate(data.test) + ';' + barakgay.generate(data.update);
+    vdecl[3] = escodegen.generate(data.init) + ';' + escodegen.generate(data.test) + ';' + escodegen.generate(data.update);
     vdecl[4] = '';
     thing.push(vdecl);
-    barakhomo(data.body);
+    makeArr(data.body);
 }
 
 function parseAssExpr(data) {
     let vdecl = [5];
     vdecl[0] = data.loc.start.line;
-    vdecl[1] = data.type;
+    vdecl[1] = 'assignment expression';
     vdecl[2] = data.left.name;
     vdecl[3] = '';
-    vdecl[4] = barakgay.generate(data.right);
+    vdecl[4] = escodegen.generate(data.right);
     thing.push(vdecl);
 }
 
--- test/code-analyzer.test.js
+++ test/code-analyzer.test.js
@@ -12,70 +12,70 @@ describe('The javascript parser', () => {
     it('is parsing a simple variable declaration correctly', () => {
         assert.equal(
             JSON.stringify(parseCode('let a;')),
-            '[[1,"VariableDeclarator","a","",""]]'
+            '[[1,"variable declaration","a","",""]]'
         );
     });
 
     it('is parsing a simple variable declaration with value correctly', () => {
         assert.equal(
             JSON.stringify(parseCode('let a = 1;')),
-            '[[1,"VariableDeclarator","a","","1"]]'
+            '[[1,"variable declaration","a","","1"]]'
         );
     });
 
     it('is parsing a simple func and return declarations correctly', () => {
         assert.equal(
             JSON.stringify(parseCode('function test (x) {return x}')),
-            '[[1,"FunctionDeclaration","test","",""],[1,"VariableDeclaration","x","",""],[1,"ReturnStatement","","","x"]]'
+            '[[1,"function declaration","test","",""],[1,"variable declaration","x","",""],[1,"return statement","","","x"]]'
         );
     });
 
     it('is parsing a simple while correctly', () => {
         assert.equal(
             JSON.stringify(parseCode('while (x<1) \n{x=1}')),
-            '[[1,"WhileStatement","","x < 1",""],[2,"AssignmentExpression","x","","1"]]'
+            '[[1,"while statement","","x < 1",""],[2,"assignment expression","x","","1"]]'
         );
     });
 
     it('is parsing a simple if correctly', () => {
         assert.equal(
             JSON.stringify(parseCode('if (x===3) {x=3}')),
-            '[[1,"IfStatement","","x === 3",""],[1,"AssignmentExpression","x","","3"]]'
+            '[[1,"if statement","","x === 3",""],[1,"assignment expression","x","","3"]]'
         );
     });
 
     it('is parsing if-else correctly', () => {
         assert.equal(
             JSON.stringify(parseCode('if (x===3) {x=3}\nelse {x=4}')),
-            '[[1,"IfStatement","","x === 3",""],[1,"AssignmentExpression","x","","3"],[2,"AssignmentExpression","x","","4"]]'
+            '[[1,"if statement","","x === 3",""],[1,"assignment expression","x","","3"],[2,"assignment expression","x","","4"]]'
         );
     });
 
     it('is parsing if-else-if correctly', () => {
         assert.equal(
             JSON.stringify(parseCode('if (x===3) {x=3}\nelse if (x===4) {x=4}\nelse {x=5}')),
-            '[[1,"IfStatement","","x === 3",""],[1,"AssignmentExpression","x","","3"],[2,"ElseIfStatement","","x === 4",""],[2,"AssignmentExpression","x","","4"],[3,"AssignmentExpression","x","","5"]]'
+            '[[1,"if statement","","x === 3",""],[1,"assignment expression","x","","3"],[2,"else if statement","","x === 4",""],[2,"assignment expression","x","","4"],[3,"assignment expression","x","","5"]]'
         );
     });
 
     it('is parsing if-if correctly', () => {
         assert.equal(
             JSON.stringify(parseCode('function a () {\nif (x===3) {x=3}\nelse {x=4}\nif (x===4) {x=4}}')),
-            '[[1,"FunctionDeclaration","a","",""],[2,"IfStatement","","x === 3",""],[2,"AssignmentExpression","x","","3"],[3,"AssignmentExpression","x","","4"],[4,"IfStatement","","x === 4",""],[4,"AssignmentExpression","x","","4"]]'
+            '[[1,"function declaration","a","",""],[2,"if statement","","x === 3",""],[2,"assignment expression","x","","3"],[3,"assignment expression","x","","4"],[4,"if statement","","x === 4",""],[4,"assignment expression","x","","4"]]'
         );
     });
 
     it('is parsing a simple for correctly', () => {
         assert.equal(
             JSON.stringify(parseCode('for (x=1;x<10;x++)\n{i=2}')),
-            '[[1,"ForStatement","","x = 1;x < 10;x++",""],[2,"AssignmentExpression","i","","2"]]'
+            '[[1,"for statement","","x = 1;x < 10;x++",""],[2,"assignment expression","i","","2"]]'
         );
     });
 
     it('is parsing a simple assignment correctly', () => {
         assert.equal(
             JSON.stringify(parseCode('x=1;')),
-            '[[1,"AssignmentExpression","x","","1"]]'
+            '[[1,"assignment expression","x","","1"]]'
         );
     });
 });
